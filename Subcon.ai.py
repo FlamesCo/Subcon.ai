fd9b20f1ef6ce8eccd046d529def34bcae038e34
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-10-30 13:43:59 -0700

    first vr 1.0x beta

---
+++ Subcon.ai.py
@@ -0,0 +1,14 @@
+## this is ai nueral network that generates perosnalized code in any coding language using python 3.0x
+print("What language would you like to code in?")
+ 
+lang = input()
+# select 3 languages
+if lang == "python":
+    print("python is a great language!")
+elif lang == "java":
+    print("java is a great language!")
+elif lang == "c++":
+    print("c++ is a great language!")
+else:
+    print("I don't know that language!")
+    exit()
